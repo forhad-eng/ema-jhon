85870b239f8408b386c0a139c8792849d3427431
forhad-eng <forhadu.cse@gmail.com>
2022-03-22 02:55:04 +0600

    first release

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,9 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import './Product.css'
 
-const Product = ({ product }) => {
+const Product = ({ product, addHandler }) => {
     const { name, img, price, seller, ratings } = product
     return (
         <div className="product">
@@ -10,9 +12,13 @@ const Product = ({ product }) => {
                 <p className="product-name">{name}</p>
                 <p className="product-price">Price: {price}</p>
                 <p className="product-manufact">Manufacturer: {seller}</p>
-                <p className="product-rating">rating: {ratings} star</p>
+                <p className="product-rating">Rating: {ratings} star</p>
             </div>
-            <button><p>Add to Cart</p></button>
+            <button onClick={() => addHandler(product)}>
+                <p>
+                    Add to Cart <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+                </p>
+            </button>
         </div>
     )
 }
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,23 +1,49 @@
+import { faArrowRight, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useEffect, useState } from 'react'
 import Product from '../Product/Product'
 import './Shop.css'
 
 const Shop = () => {
+    const [cart, setCart] = useState([])
     const [products, setProducts] = useState([])
     useEffect(() => {
         fetch('./products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+
+    const addToCartHandler = product => {
+        const newCart = [...cart, product]
+        setCart(newCart)
+    }
+    let price = 0
+    const charge = 100
     return (
         <div className="shop-container">
             <div className="product-container">
                 {products.map(product => (
-                    <Product product={product}></Product>
+                    <Product product={product} addHandler={addToCartHandler}></Product>
                 ))}
             </div>
             <div className="cart-container">
-                <h2>This is cart</h2>
+                <p className="order-summary">Order Summary</p>
+                <div className="cart-info">
+                    <p>Selected Item: {cart.length}</p>
+                    <p>
+                        {cart.forEach(item => (price = price + item.price))}
+                        Total Price: {price}
+                    </p>
+                    <p>Total Shipping Charge: {charge}</p>
+                    <p>Tax: {(price * 0.2).toFixed(2)}</p>
+                    <p>Grand Total: {price + price * 0.2 + charge}</p>
+                </div>
+                <button onClick={() => window.location.reload()} className="clr-cart">
+                    Clear Cart <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
+                </button>
+                <button className="rmv-cart">
+                    Remove Cart <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+                </button>
             </div>
         </div>
     )
